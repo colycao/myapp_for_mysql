1a8113d1275fbb477039ceab1bebc2d428c8796e
Coly <237549582@qq.com>
2013-06-08 12:22:25 +0800

    添加权限控制
    操作帖子时，除了index动作和view动作，其他都要验证。
    操作评论时，需要验证身份才能删除评论。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "demo", password: "demo", only: :destroy
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
